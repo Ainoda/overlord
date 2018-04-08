e7e8d13df0dd7c39e9a2fa133ce4fb30eb032ba5
lizhiping <276340452@qq.com>
2018-04-08 15:00:47 +0800

    feat(core): add pet quality

---
+++ wowPets/wow-pets-core/src/quality/quality.js
@@ -0,0 +1,9 @@
+class quality {
+  // pet品质
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+}
+
+module.exports = quality;
---
+++ wowPets/wow-pets-core/src/quality/qualityService.js
@@ -0,0 +1,24 @@
+const qualityDao = require('./qualityDao');
+
+const qualityService = {
+  insert(quality) {
+    let result;
+    if(Array.isArray(quality)){
+      result = qualityDao.insertMany(quality);
+    }else {
+      result = qualityDao.insertOne(quality);
+    }
+    return result;
+  },
+  delete(where) {
+    return qualityDao.deleteOne(where);
+  },
+  update(where, update) {
+    return qualityDao.updateOne(where, update);
+  },
+  find(where) {
+    return qualityDao.find(where);
+  }
+}
+
+module.exports = qualityService;
